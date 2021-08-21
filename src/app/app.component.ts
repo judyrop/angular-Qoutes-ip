68dbd82eaf8dd7c1e8fa2e9bbd2a1f89d34dd542
Your Name <youremail@domain.com>
2021-08-21 15:30:01 +0300

    created class to my app and added some features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-
+import {Quote} from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes :string[];
+  quotes: Quote[]=[
+    {id:1,name:'The way i see it,if you want the rainbow, you gotta put up with the rain'},
+       {id:2,name:'Choose to be happy'},
+    {id:3,name:'He who is not courageous to take risks will accomplish nothing in life'}
+  
+  ];
 
-  constructor(){
-    this.quotes = ['The way i see it,if you want the rainbow, you gotta put up with the rain','Choose to be happy', 'He who is not courageous to take risks will accomplish nothing in life']
-  }
+  // constructor(){
+  //   this.quotes = ['The way i see it,if you want the rainbow, you gotta put up with the rain','Choose to be happy', 'He who is not courageous to take risks will accomplish nothing in life']
+  // }
 
 }
