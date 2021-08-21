9835e36fee0c282798ef35985dfc50ef99261f69
Your Name <youremail@domain.com>
2021-08-21 13:05:10 +0300

    added title to my app

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+Quotes = 'The way i see it,if you want the rainbow, you gotta put up with the rain.'
+}
